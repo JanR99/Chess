3c832899863858eb11259ce5169e9a3c494be802
Jan <jan.rehnert@hotmail.de>
2026-07-30 15:23:43 +0200

    fixed pawn transform to queen

--- src/main/java/game/ChessGame.java
+++ src/main/java/game/ChessGame.java
@@ -90,12 +90,14 @@ public class ChessGame {
 
     private void transformPawnToQueenIfLastRow(Piece piece) {
         if (!(piece instanceof Pawn)) {
+            System.out.println("Ist kein Pawn");
             return;
         }
-        if (piece.getColor().equals(Color.WHITE) && piece.getPosition().getRow() == Position.LAST_ROW
-                || piece.getColor().equals(Color.BLACK) && piece.getPosition().getRow() == Position.FIRST_ROW) {
+        if (piece.getColor().equals(Color.WHITE) && piece.getPosition().getRow() == Position.FIRST_ROW
+                || piece.getColor().equals(Color.BLACK) && piece.getPosition().getRow() == Position.LAST_ROW) {
             // TODO let the User decide which Piece
             board.setPiece(piece.getPosition(), new Queen(piece.getPosition(), piece.getColor()));
+            System.out.println( board.getPiece(piece.getPosition()));
         }
     }
 
